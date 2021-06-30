1fac8d2abd2c162209025ad13af837b245c15528
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-06-30 20:34:47 +0800

    .

--- crates/printer/src/lib.rs
+++ crates/printer/src/lib.rs
@@ -1,6 +1,8 @@
 //! This crate provides the feature of diplaying the information of filtered lines
 //! by printing them to stdout in JSON format.
 
+mod legacy;
+
 use std::collections::HashMap;
 
 use icon::{IconPainter, ICON_LEN};
@@ -183,76 +185,6 @@ fn truncate_text(
 //
 // if display_width(text) > max_width - 2
 
-fn truncate_line_impl(
-    winwidth: usize,
-    line: &str,
-    indices: &[usize],
-    skipped: Option<usize>,
-) -> Option<(String, Vec<usize>)> {
-    let last_idx = indices.last()?;
-    if *last_idx > winwidth {
-        let mut start = *last_idx - winwidth;
-        if start >= indices[0] || (indices.len() > 1 && *last_idx - start > winwidth) {
-            start = indices[0];
-        }
-        let line_len = line.len();
-        // [--------------------------]
-        // [-----------------------------------------------------------------xx--x--]
-        for _ in 0..3 {
-            if indices[0] - start >= DOTS.len() && line_len - start >= winwidth {
-                start += DOTS.len();
-            } else {
-                break;
-            }
-        }
-        let trailing_dist = line_len - last_idx;
-        if trailing_dist < indices[0] - start {
-            start += trailing_dist;
-        }
-        let end = line.len();
-        let left_truncated = if let Some(n) = skipped {
-            let icon: String = line.chars().take(n).collect();
-            format!("{}{}{}", icon, DOTS, utf8_str_slice(&line, start, end))
-        } else {
-            format!("{}{}", DOTS, utf8_str_slice(&line, start, end))
-        };
-
-        let offset = line_len.saturating_sub(left_truncated.len());
-
-        let left_truncated_len = left_truncated.len();
-
-        let (truncated, max_index) = if left_truncated_len > winwidth {
-            if left_truncated_len == winwidth + 1 {
-                (
-                    format!("{}.", utf8_str_slice(&left_truncated, 0, winwidth - 1)),
-                    winwidth - 1,
-                )
-            } else {
-                (
-                    format!(
-                        "{}{}",
-                        utf8_str_slice(&left_truncated, 0, winwidth - 2),
-                        DOTS
-                    ),
-                    winwidth - 2,
-                )
-            }
-        } else {
-            (left_truncated, winwidth)
-        };
-
-        let truncated_indices = indices
-            .iter()
-            .map(|x| x - offset)
-            .take_while(|x| *x < max_index)
-            .collect::<Vec<_>>();
-
-        Some((truncated, truncated_indices))
-    } else {
-        None
-    }
-}
-
 /// Long matched lines can cause the matched items invisible.
 ///
 /// # Arguments
@@ -303,7 +235,7 @@ pub fn truncate_grep_lines(
             lnum += 1;
 
             if let Some((truncated, truncated_indices)) =
-                truncate_line_impl(winwidth, &line, &indices, skipped)
+                crate::legacy::truncate_line_impl(winwidth, &line, &indices, skipped)
             {
                 truncated_map.insert(lnum, line);
                 (truncated, truncated_indices)
